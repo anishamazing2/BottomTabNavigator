32dc9d1732c2bf9de011fddf29e3ecabf9a54ea1
anishamazing2 <73046127+anishamazing2@users.noreply.github.com>
2021-07-06 17:49:36 -0700

    Add files via upload

---
+++ screens/Facebook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+
+
+
+export default class FacebookScreen extends React.Component {
+  render(){
+    return (
+
+      <Text>FACEBOOK</Text>
+
+    );
+  }
+}
---
+++ screens/Instagram.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+
+
+
+export default class InstagramScreen extends React.Component {
+  render(){
+    return (
+
+      <Text>INstagram</Text>
+
+    );
+  }
+}
